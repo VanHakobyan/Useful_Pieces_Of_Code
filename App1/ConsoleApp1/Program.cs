409dc76ac281e56f653640ac555582de162fe6e1
Vanik Hakobyan <vanhakobyan1996@gmail.com>
2017-10-13 09:21:07 +0400

    corrected :v:

--- App1/ConsoleApp1/Program.cs
+++ App1/ConsoleApp1/Program.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace ReferensType
 {
     class Program
     {
