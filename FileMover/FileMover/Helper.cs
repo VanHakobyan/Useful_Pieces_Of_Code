6590a193043e41b9165be2de0e1f63efbc57f393
van hakobyan <van19962013@mail.ru>
2018-02-01 16:01:11 +0400

    add system file mover  App

---
+++ FileMover/FileMover/Helper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileMover
+{
+    static class Helper
+    {
+        public static IEnumerable<string> GetFileNames(string currentDir)
+        {
+            return Directory.GetFiles(currentDir).ToList();
+        }
+        public static string GetFileSize(string filename)
+        {
+            string[] sizes = { "B", "KB", "MB", "GB", "TB" };
+            double len = new FileInfo(filename).Length;
+            int order = 0;
+            while (len >= 1024 && order < sizes.Length - 1)
+            {
+                order++;
+                len = len / 1024;
+            }
+            // Adjust the format string to your preferences. For example "{0:0.#}{1}" would
+            // show a single decimal place, and no space.
+            return $"{len:0.##} {sizes[order]}";
+        }
+        public static string GetTempDirectory()
+        {
+            return Directory.CreateDirectory(Path.Combine(@"D:\", Path.GetRandomFileName())).FullName;
+        }
+    }
+}
---
+++ FileMover/FileMover/Mover.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileMover
+{
+    public class Mover
+    {
+        public void MoveZipedFile()
+        {
+            var moveBegin = ConfigurationManager.AppSettings["Begin"];
+            var movEnd = ConfigurationManager.AppSettings["End"];
+            var rootDirectories = Directory.GetDirectories(moveBegin).Where(x => x.Contains("Partner_"));
+            var filesToMove = new List<string>();
+            foreach (var rootDirectory in rootDirectories)
+            {
+                var list = Directory.GetFiles(rootDirectory).Where(x => x.EndsWith(".zip")).ToList();
+                if (list.Count != 0) filesToMove.AddRange(list);
+            }
+            if (filesToMove.Count == 0) return;
+            var moveFolderName = string.Join("", filesToMove.First().Split('.')[1].Take(10));
+            if (!Directory.GetDirectories(movEnd).Any(x => x.Contains(moveFolderName))) Directory.CreateDirectory($@"{movEnd}\{moveFolderName}");
+            foreach (var fileToMove in filesToMove)
+            {
+                if (!fileToMove.Contains(moveFolderName)) continue;
+                var destinationFile = $@"{movEnd}\{moveFolderName}\{fileToMove.Split(new[] { "\\" }, StringSplitOptions.RemoveEmptyEntries).Last()}";
+                File.Move(fileToMove, destinationFile);
+            }
+        }
+
+        public void ZipAndMove()
+        {
+            var currentDir = ConfigurationManager.AppSettings["Begin"];
+            var toMove = ConfigurationManager.AppSettings["End"];
+            var extension = "zip";
+            var fileNames = Helper.GetFileNames(currentDir);
+            foreach (var fileName in fileNames)
+            {
+                var fileSize = Helper.GetFileSize(fileName);
+                if (double.Parse(fileSize.Split(' ').First()) > 10 && fileSize.Split(' ').Last() == "MB")
+                {
+                    var destFileName = fileName.Replace(currentDir, string.Empty);
+                    var tempDirectory = Helper.GetTempDirectory();
+                    var extensionMover = Path.GetExtension(fileName);
+                    var name = Path.GetFileNameWithoutExtension(fileName);
+                    var toMoveTemp = $@"{tempDirectory}\{name}{extensionMover}";
+                    File.Move(fileName, toMoveTemp);
+                    ZipFile.CreateFromDirectory(tempDirectory, $@"{toMove}{destFileName}.{extension}", CompressionLevel.Optimal,
+                        true, Encoding.UTF8);
+                    File.Delete(toMoveTemp);
+                    Directory.Delete(tempDirectory);
+                }
+            }
+        }
+    }
+}
