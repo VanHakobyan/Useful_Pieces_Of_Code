59527949f2ba5d51392a226441230463f7b10c28
vanhakobyan <van19962013@mail.ru>
2018-02-03 19:44:10 +0400

    Special for Mane

---
+++ CustomComplexType/CustomComplexType/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomComplexType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ComplexNumber complexNumber1 = new ComplexNumber("1", "12");
+            ComplexNumber complexNumber2 = new ComplexNumber("32", "16");
+            ComplexNumber cSum = complexNumber1.Sum(complexNumber2);
+        }
+    }
+}
