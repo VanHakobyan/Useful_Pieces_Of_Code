2e7959d67a2e93d17692554dcc1cb0c282202dc9
van hakobyan <van19962013@mail.ru>
2018-02-05 17:01:44 +0400

    simple smtp client

---
+++ SMTP.MessageSender/MesageSender/EmailSender.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Mail;
+
+namespace MesageSender
+{
+    public class EmailSender
+    {
+        public void SendEmail(Contact contact, string content)
+        {
+
+            using (var msg = new MailMessage())
+            {
+
+                msg.To.Add(contact.Email);
+                msg.Subject = $"Dear member - {contact.FirstName} {contact.LastName}";
+                //msg.IsBodyHtml = true;
+                msg.Body = content;
+
+                var client = new SmtpClient();
+                try
+                {
+                    client.Send(msg);
+                }
+                catch (Exception ex)
+                {
+                    File.AppendAllText(@"D:\emailSender.log",$"Message: {ex.Message} {Environment.NewLine}Inner: {ex.InnerException}{Environment.NewLine}");
+                }
+
+            }
+
+        }
+
+        public void SendEmailList(List<Contact> contacts, string content)
+        {
+            foreach (var contact in contacts)
+                SendEmail(contact, content);
+        }
+    }
+}
---
+++ SMTP.MessageSender/SMTP.MessageSender/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MesageSender;
+
+namespace SMTP.MessageSender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var emailSender = new EmailSender();
+            emailSender.SendEmail(new Contact { FirstName = "yourName", Email = "toSendEmail", LastName = "Hakobyan" }, "Message");
+        }
+    }
+}
