53b1586ebfd305bf1b8cda949282e99f1d3d3a4d
van hakobyan <van19962013@mail.ru>
2018-04-27 19:15:27 +0400

    comment json file reader :one:

--- WebProxyManiputation/Helpers/SimpleProxys.cs
+++ WebProxyManiputation/Helpers/SimpleProxys.cs
@@ -14,7 +14,7 @@ namespace Helpers
         {
             try
             {
-                var jsonString = File.ReadAllText($@"{Environment.CurrentDirectory}\Proxies.json".Replace("\\bin\\Debug", "").Replace("\\bin\\Release", ""));
+                var jsonString = "";//File.ReadAllText($@"{Environment.CurrentDirectory}\Proxies.json".Replace("\\bin\\Debug", "").Replace("\\bin\\Release", ""));
                 var jsonObject = JsonConvert.DeserializeObject<List<ProxyModel>>(jsonString);
                 return jsonObject.Take(100);
             }
