b7906520b5121a20bc60106f01822057f66e04d7
van hakobyan <van19962013@mail.ru>
2018-05-03 18:11:06 +0400

    commented

--- WebProxyManiputation/Helpers/SimpleProxys.cs
+++ WebProxyManiputation/Helpers/SimpleProxys.cs
@@ -2,8 +2,6 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Newtonsoft.Json;
 
 namespace Helpers
@@ -20,7 +18,7 @@ namespace Helpers
             }
             catch (Exception ex)
             {
-                File.AppendAllText(@"D:\proxyWpf.log", $"{Environment.CurrentDirectory}");
+                //File.AppendAllText(@"D:\proxyWpf.log", $"{Environment.CurrentDirectory}");
                 return null;
             }
         }
