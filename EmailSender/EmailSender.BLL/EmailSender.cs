87b1f0cc540a9b34c441b317f062ccb8ad29eb4b
vanhakobyan1996 <vanhakobyan1996@gmail.com>
2019-11-21 21:56:00 +0400

    *

--- EmailSender/EmailSender.BLL/EmailSender.cs
+++ EmailSender/EmailSender.BLL/EmailSender.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Net;
 using System.Net.Http;
 using System.Net.Mail;
+using System.Net.Mime;
 using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
@@ -17,9 +18,9 @@ namespace EmailSender.BLL
 
         public EmailSender()
         {
-            var json = File.ReadAllText($"{Directory.GetCurrentDirectory()}/contacts.json");
-            var contacts = JsonConvert.DeserializeObject<List<Profile>>(json);
-            FactoryModel(contacts);
+            //var json = File.ReadAllText($"{Directory.GetCurrentDirectory()}/contacts.json");
+            //var contacts = JsonConvert.DeserializeObject<List<Profile>>(json);
+            //FactoryModel(contacts);
         }
 
         private void FactoryModel(List<Profile> contacts)
@@ -61,6 +62,45 @@ namespace EmailSender.BLL
                 msg.IsBodyHtml = true;
                 msg.Body = GetMessageText(contact);
 
+                var client = new SmtpClient
+                {
+                    Host = "smtp.gmail.com",
+                    Port = 587,
+                    EnableSsl = true,
+                    DeliveryMethod = SmtpDeliveryMethod.Network,
+                    UseDefaultCredentials = false,
+                    Credentials = new NetworkCredential("newsvh2018@gmail.com", "*")
+                };
+
+                try
+                {
+                    client.Send(msg);
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception(ex.Message);
+
+                }
+
+            }
+
+        }
+
+        public void SendEmail()
+        {
+
+            using (var msg = new MailMessage())
+            {
+
+                msg.From = new MailAddress("newsvh2018@gmail.com");
+
+                msg.To.Add("*@gmail.com");
+                //msg.To.Add("vanhakobyan1996@gmail.com");
+                msg.Subject = "VH Development";
+                msg.IsBodyHtml = true;
+                msg.Body = File.ReadAllText(@"*");
+                msg.BodyTransferEncoding = TransferEncoding.QuotedPrintable;
+                msg.Headers.Add("Content-Type", "text/html; charset='iso-8859-1'");
                 var client = new SmtpClient
                 {
                     Host = "smtp.gmail.com",
---
+++ EmailSender/Sender/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EmailSender.BLL.EmailSender  emailSender=new EmailSender.BLL.EmailSender();
+            emailSender.SendEmail();
+        }
+    }
+}
